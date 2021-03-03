cd266a05c26af6c28464838de3ac6f8b24c74de2
arpitp07 <arpitparihar07@uchicago.edu>
2021-03-02 21:17:09 -0600

    Assignment 7 Commit

--- Week 6/Week_6_hw_HR.py
+++ Week 6/Week_6_hw_HR.py
@@ -71,7 +71,10 @@ def find_node(start, key):
     curr = start
     while True:
         if distance(curr.id, key) + distance(key, curr.next.id) == distance(curr.id, curr.next.id):
-            return curr.next
+            if distance(key, curr.next.id) == 0:
+                return curr.next  
+            else:
+                return curr
         curr = curr.next
 
 def store(start, key, value):
@@ -86,11 +89,11 @@ def lookup(start, key):
     return node.data[key]
 
 def update(node, k=k_bit):
-    # updates the finger table for given node
+    # updates the finger table for given node    
     for i in range(k):
         finger_key = (node.id + 2**i)%2**k
-        node.finger[i+1] = find_node(node, finger_key)
-
+        node.finger[i] = find_node(node, finger_key)
+    
 def find_finger(node, key, k=k_bit):
     # use the nodes finger table to get the node closest to the key
     curr = node
@@ -98,18 +101,21 @@ def find_finger(node, key, k=k_bit):
     dist = distance(curr.id, key)
     while True:
         if distance(curr.id, key) + distance(key, curr.next.id) == distance(curr.id, curr.next.id):
-            return curr.next
+            if distance(key, curr.next.id) == 0:
+                return curr.next  
+            else:
+                return curr
         elif distance(curr.finger[i].id, key) > dist:
             curr = curr.finger[i-1]
             dist = distance(curr.id, key)
             i=1
-        elif i==32:
+        elif i==31:
             curr = curr.finger[i]
             dist = distance(curr.id, key)
             i=1
         else:
             i+=1
-
+    
 def finger_lookup(start, key):
     # find the value stored at the key using finger table lookups starting with node "start"
     node = find_finger(start, key)
@@ -122,10 +128,10 @@ def finger_store(start, key, value):
     return
     
     
-def case3():
+def case3(fptr):
     # what is the largest possible node id in the network if k=32?
     answer = 2**32
-    print(str(answer)+ '\n')
+    fptr.write(str(answer)+ '\n')
     pass
 
 def setup1():
@@ -142,47 +148,47 @@ def setup1():
     return cll
 
 
-def case1():
+def case1(fptr):
     node = Node({}, k_bit)
-    print(str(node.id) + '\n')
-    print(str(node.data) + '\n') 
-    print(str(node.next) + '\n')
-    print(str(node.finger) + '\n')
-
-def case2():
+    fptr.write(str(node.id) + '\n')
+    fptr.write(str(node.data) + '\n') 
+    fptr.write(str(node.next) + '\n')
+    fptr.write(str(node.finger) + '\n')
+    
+def case2(fptr):
     cll = CircularLinkedList()
-    print(str(cll.head) + '\n')
+    fptr.write(str(cll.head) + '\n')
     l = [cll.sorted_insert(Node({}, k_bit)) for x in range(10)]
     cllist = cll.get_list()
     for e in cllist:
         for d in e:
-            print(str(d) + ' ')
-        print('\n')
-
-def case4():
+            fptr.write(str(d) + ' ')
+        fptr.write('\n')
+        
+def case4(fptr):
     d1 = distance(10, 10)
     d2 = distance(10, 100)
     d3 = distance(100, 10)
-    print(str(d1) + '\n')
-    print(str(d2) + '\n')
-    print(str(d3) + '\n')
-
-def case5():
+    fptr.write(str(d1) + '\n')
+    fptr.write(str(d2) + '\n')
+    fptr.write(str(d3) + '\n')
+    
+def case5(fptr):
     cll = CircularLinkedList()
     l = [cll.sorted_insert(Node({}, k_bit)) for x in range(10)]
     node = find_node(cll.head, 462568970)
-    print(str(node.id) + '\n')
-    print(str(node.data) + '\n')
-    print(str(node.next.id) + '\n')
-
-def case6():
+    fptr.write(str(node.id) + '\n')
+    fptr.write(str(node.data) + '\n')
+    fptr.write(str(node.next.id) + '\n')
+    
+def case6(fptr):
     cll = CircularLinkedList()
     l = [cll.sorted_insert(Node({}, k_bit)) for x in range(10)]
     store(cll.head, 1606153229, 4)
     value = lookup(cll.head, 1606153229)
-    print(str(value) + '\n')
-
-def case7():
+    fptr.write(str(value) + '\n')
+    
+def case7(fptr):
     # tests speed of regular insert
     arr = [x for x in range(0, 2 ** 12)]
     start = CircularLinkedList()
@@ -192,61 +198,35 @@ def case7():
         start.sorted_insert(temp)
     process_time = time.time() - start_time
     print("SortedInsert took {} seconds".format(process_time))
-
-def case8():
+    
+def case8(fptr):
     cll = setup1()
     node = find_node(cll.head, 344973245)
-    print(str(node.data)+'\n')
+    fptr.write(str(node.data)+'\n')
     n28 = node.finger[28]
     n30 = node.finger[30]
-    print(str(n28.data)+ '\n')
-    print(str(n30.data) + '\n')
-
-def case9():
+    fptr.write(str(n28.data)+ '\n')
+    fptr.write(str(n30.data) + '\n')
+    
+def case9(fptr):
     cll = setup1()
     value = finger_lookup(cll.head, 344973245)
-    print(str(value)+'\n')
+    fptr.write(str(value)+'\n')
     new_k = 2415140493
     finger_store(cll.head, new_k, 701)
     val = finger_lookup(cll.head, new_k)
-    print(str(val) + '\n')
+    fptr.write(str(val) + '\n')
     node = find_node(cll.head, new_k)
-    print(str(node.data) + '\n')
-    print(str(node.id) + '\n')
+    fptr.write(str(node.data) + '\n')
+    fptr.write(str(node.id) + '\n')
+    
+    
 
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-#     case_num = input()
-#     globals()['case' + str(case_num)]()
-#     .close()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    case_num = input()
+    
+    globals()['case' + str(case_num)](fptr)
 
-########## QC ##########
-
-cll = setup1()
-l_node = []
-curr = cll.head
-while True:
-    l_node.append(curr)
-    curr = curr.next
-    if curr == cll.head: break
-l_id = [x.id for x in l_node]
-
-index = 3
-l_node[index].finger = {}
-update(l_node[index])
-
-node = find_finger(l_node[29], l_node[28].id-1)
-# node = find_node(l_node[0], cll.head.id)
-print(node.data, node.id)
-
-########## Test Cases ##########
-
-# case1()    
-# case2()
-# case3()
-# case4()
-# case5()
-# case6()
-# case7()
-# case8()
-# case9()
+    fptr.close()
